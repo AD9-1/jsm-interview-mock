04cf459cdaf54d417dc1b44ddcb8682110f96e1e
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-01-03 22:48:35 -0500

    InterviewCard progressing

--- types/index.t.ts
+++ types/index.t.ts
@@ -17,3 +17,18 @@ techstack:string[],
 type:string,
 createdAt:string
 }
+
+interface Feedback {
+  id: string;
+  interviewId: string;
+  totalScore: number;
+  categoryScores: Array<{
+    name: string;
+    score: number;
+    comment: string;
+  }>;
+  strengths: string[];
+  areasForImprovement: string[];
+  finalAssessment: string;
+  createdAt: string;
+}
