5e1eac5a2943d754dae086846cb011b0a6c120ab
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-01-03 21:08:35 -0500

    InterviewCard progressing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,32 +6,47 @@ import { VscRobot } from "react-icons/vsc";
 
 const RootPage = () => {
   return (
-    <div className="flex justify-between place-items-center bg-[#E0D1D1] shadow-2xl rounded-3xl p-5 md:p-10">
-      <section className="flex flex-col gap-4">
-        <h1 className="text-4xl font-semibold">
-          Ace your interviews with AI-powered practice
-        </h1>
-        <p className="text-2xl">
-          Practice with AI-powered mock interviews to improve your skills and
-          confidence.
-        </p>
-        <Button
-          asChild
-          className="p-5 w-full text-xl  md:w-fit bg-gradient-to-r
+    <>
+      <div className="flex justify-between place-items-center bg-[#E0D1D1] shadow-2xl rounded-3xl p-5 md:p-10">
+        <section className="flex flex-col gap-4">
+          <h1 className="text-4xl font-semibold">
+            Ace your interviews with AI-powered practice
+          </h1>
+          <p className="text-2xl">
+            Practice with AI-powered mock interviews to improve your skills and
+            confidence.
+          </p>
+          <Button
+            asChild
+            className="p-5 w-full text-xl  md:w-fit bg-gradient-to-r
            from-[#231e1e] via-[#69573e] to-[#382a17] text-white hover:scale-105 "
-        >
-          <Link href="/interview">Start An Interview</Link>
-        </Button>
-      </section>
-      <div className=" hidden md:block">
-        <Image
-          src="/robot-svgrepo.svg"
-          alt="Robot Image"
-          width={500}
-          height={400}
-        />
+          >
+            <Link href="/interview">Start An Interview</Link>
+          </Button>
+        </section>
+        <div className=" hidden md:block">
+          <Image
+            src="/robot-svgrepo.svg"
+            alt="Robot Image"
+            width={500}
+            height={400}
+          />
+        </div>
       </div>
-    </div>
+      <section className="flex flex-col gap-4 mt-6 ml-3">
+        <h2 className="text-4xl font-semibold">Your Interview</h2>
+        <div>
+          <p className="text-lg">You haven't taken any interview yet.</p>
+        </div>
+      </section>
+      <section className="flex flex-col gap-4 mt-6 ml-3">
+        <h2 className="text-4xl font-semibold">Take an Interview</h2>
+
+        <div>
+          <p className="text-lg">There are no interviews available.</p>
+        </div>
+      </section>
+    </>
   );
 };
 
