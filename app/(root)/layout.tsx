85b3d2de0bb401fdbc6344a2b9d5cafb73b66508
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-01-03 20:15:25 -0500

    RootLayout design

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,8 +5,8 @@ import { FcAssistant } from "react-icons/fc";
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
 
-      <div className="flex flex-col bg-gradient-to-t from-[#E0D1D1] via-[#d0c7c5] to-[#e5d6c2]
-       max-w-7xl mx-auto py-8 mt-3 rounded-[50px]  px-4">
+      <div className="flex flex-col bg-gradient-to-t from-[#E0D1D1] via-[#cea79e] to-[#e5d6c2]
+       max-w-7xl mx-auto py-8 rounded-bl-[50px] rounded-br-[50px] px-4">
         <nav>
           <Link href="/" className="flex gap-2 items-center md:justify-evenly">
            <FcAssistant className="h-20 w-20 md:h-30 md:w-30"/>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
+import { VscRobot } from "react-icons/vsc";
 
 const RootPage = () => {
-  return <div>helooo</div>;
+  return (
+    <div className="flex justify-between place-items-center bg-[#E0D1D1] shadow-2xl rounded-3xl p-5 md:p-10">
+      <section className="flex flex-col gap-4">
+        <h1 className="text-4xl font-semibold">
+          Ace your interviews with AI-powered practice
+        </h1>
+        <p className="text-2xl">
+          Practice with AI-powered mock interviews to improve your skills and
+          confidence.
+        </p>
+        <Button
+          asChild
+          className="p-5 w-full text-xl  md:w-fit bg-gradient-to-r
+           from-[#231e1e] via-[#69573e] to-[#382a17] text-white hover:scale-105 "
+        >
+          <Link href="/interview">Start An Interview</Link>
+        </Button>
+      </section>
+      <div className=" hidden md:block">
+        <Image
+          src="/robot-svgrepo.svg"
+          alt="Robot Image"
+          width={500}
+          height={400}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RootPage;
