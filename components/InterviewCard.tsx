75846c19290a9b16b24d0f3f4e0c79d06dc1ef18
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-01-07 19:18:53 -0500

    InterviewCard progressing

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -16,25 +16,46 @@ const InterviewCard = ({
   const formattedDate = dayjs(
     feedback?.createdAt || createdAt || Date.now()
   ).format("MMM D, YYYY");
-  const getRandomImage=()=>{
-return interviewImage[Math.floor(Math.random() * interviewImage.length)];
-  }
+  const getRandomImage = () => {
+    return interviewImage[Math.floor(Math.random() * interviewImage.length)];
+  };
   return (
     <div
       className=" bg-gradient-to-r from-[#c9b5a1] via-[#ecd7b9] to-[#eac087]
    shadow-xl max-sm:w-full w-[360px] min-h-96 rounded py-.5 px-1 relative"
     >
-      <div className="bg-gradient-to-t from-[#4f473e] via-[#be9255] to-[#eac087]
-       w-full p-2 shadow-xl rounded-[20px] my-1 ">
+      <div
+        className="bg-gradient-to-t from-[#4f473e] via-[#be9255] to-[#eac087]
+       w-full p-2 shadow-xl rounded-[10px] my-1 "
+      >
         <div className="">
-          <div className=" absolute top-0 right-0 bg-gradient-to-r from-[#a88461]
-           to-[#d2b790] p-1 rounded-bl-lg">
+          <div
+            className=" absolute top-0 right-0 bg-gradient-to-r from-[#a88461]
+           to-[#d2b790] p-1 rounded-bl-lg"
+          >
             <p className="text-md font-semibold">{normalizedType}</p>
           </div>
-          <Image src={getRandomImage()} width={50} height={50} 
-          className="size-[50px] rounded object-fill" alt="logo"></Image>
-        <h3 className=" text-lg font-stretch-50%">{role} Interview</h3>
-        
+          <Image
+            src={getRandomImage()}
+            width={50}
+            height={50}
+            className="size-[50px] rounded object-fill"
+            alt="logo"
+          ></Image>
+          <h3 className=" text-lg font-stretch-50% mt-2">{role} Interview</h3>
+          <section className="flex gap-5 flex-wrap">
+            <p className="text-stone-300 ">{formattedDate}</p>
+            <div className="flex gap-2">
+              <p className="text-stone-50 text-sm">
+                Score: {feedback?.totalScore || "--"}/100
+              </p>
+            </div>
+          </section>
+
+          <h4 className="mt-2 font-semibold">
+            {feedback?.finalAssessment ||
+              "You havent taken the interview yet.Take it now skills"}
+          </h4>
         </div>
       </div>
     </div>
