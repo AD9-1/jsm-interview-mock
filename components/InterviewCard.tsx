7d03880cfbaa8d26f4138188ba57e981f4a2778d
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-01-08 16:33:33 -0500

    InterviewCard progressing

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -24,12 +24,12 @@ const InterviewCard = ({
   };
   return (
     <div
-      className=" bg-gradient-to-r from-[#c9b5a1] via-[#ecd7b9] to-[#eac087]
-   shadow-xl max-sm:w-full w-[360px] min-h-96 rounded py-.5 px-1 relative"
+      className=" bg-gradient-to-r from-[#78441345] via-[#b7976d45] to-[#5b4d39]
+   shadow-xl max-sm:w-full max-w-[320px] min-h-72 rounded-[10px] py-.5 px-1 relative overflow-y-auto "
     >
       <div
         className="bg-gradient-to-t from-[#4f473e] via-[#be9255] to-[#eac087]
-       w-full p-2 shadow-xl rounded-[10px] my-1 "
+       w-full h-72 p-2 shadow-xl rounded-[10px] my-1 "
       >
         <div
           className=" absolute top-0 right-0 bg-gradient-to-r from-[#a88461]
@@ -41,7 +41,7 @@ const InterviewCard = ({
           src={getRandomImage()}
           width={50}
           height={50}
-          className="size-[50px] rounded object-fill"
+          className="size-[65px] rounded object-fill rounded-full "
           alt="logo"
         ></Image>
         <h3 className=" text-lg font-stretch-50% mt-2">{role} Interview</h3>
