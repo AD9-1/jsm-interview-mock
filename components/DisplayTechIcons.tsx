92b4212005f28605a29640feecb4fbe3438f9a8e
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-01-08 15:00:20 -0500

    InterviewCard progressing

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { fetcher, getTechIcon } from "../lib/utils";
+import { cn, fetcher, getTechIcon } from "../lib/utils";
 import Image from "next/image";
+import clsx from "clsx";
 const DisplayTechIcons = async ({ techstack }: { techstack: string[] }) => {
   const techIcons = await fetcher(techstack);
   console.log(techIcons);
   return (
-    <div className="flex gap-2">
-      {techIcons.map(({ item, url }) => (
-        <div key={item} className="p-2 rounded-4xl group relative bg-[#241b30]">
+    <div className="flex">
+      {techIcons.map(({ item, url }, index) => (
+        <div
+          key={item}
+          className={clsx(
+            "p-2 rounded-4xl group relative bg-[#241b30] border",
+            index > 0 && "-ml-3"
+          )}//include -ml-3 only if index is greater than 0
+        >
           <span
             className=" hidden absolute bottom-full right-0.5 group-hover:block
           bg-[#49454e] p-1 text-white text-[10px] rounded-xl"
