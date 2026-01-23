871d91c35c1c173b11242f4e8b69c6ba1436fcfe
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-01-23 18:44:58 -0500

    firebase configuration initializeFBAdmin

---
+++ firebase/admin.ts
@@ -0,0 +1,21 @@
+import { cert, getApps, initializeApp } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+const initializeFBAdmin = () => {
+  const apps = getApps;
+  if (apps.length === 0) {
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      }),
+    });
+  }
+  return {
+    auth: getAuth(),
+    db: getFirestore(),
+  };
+};
+
+export default initializeFBAdmin;
