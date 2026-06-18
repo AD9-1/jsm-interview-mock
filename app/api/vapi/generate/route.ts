e5584a35898a8cc5ec8da982e8580ee05d4f297c
Aparna Dhara <84341093+AD9-1@users.noreply.github.com>
2026-06-18 14:32:31 -0400

    Agent page change with whole transcript from user and agent

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -8,6 +8,8 @@ export async function GET() {
   return Response.json({ success: true, data: "Thank You!" }, { status: 200 });
 }
 export async function POST(request: Request) {
+  console.log("POST /api/vapi/generate called");
+
   try {
     const body = await request.json();
     const response = await generateText({
